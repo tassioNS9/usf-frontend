c30fb87189444dcb393a96740f6083f81bcd0e0b
Tassio Neves <88889043+tassioNS9@users.noreply.github.com>
2024-06-14 10:33:55 -0300

    adicionando image excluida de background de erro

--- src/views/authentication/Error.vue
+++ src/views/authentication/Error.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center items-center text-center h-100">
         <div>
-            <img src="@/assets/images/backgrounds/errorimg.svg" width="500" alt="404" />
+            <img src="@/assets/errorimg" width="500" alt="404" />
             <h1 class="pt-3">Opps!!!</h1>
             <h4 class="my-8">Esta página que você procura não foi encontrada...</h4>
             <!-- <v-btn rounded="pill" color="primary" class="mb-4"  to="/">Go Back to Home</v-btn> -->
